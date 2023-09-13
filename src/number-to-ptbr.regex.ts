284cdc05464951f52a5f073d76111d099f139f95
Joel Garcia Jr <joel.garciajr84@gmail.com>
2023-09-13 18:16:48 -0300

    Refactor convertToText for Improved Clarity and Maintainability

--- src/number-to-ptbr.regex.ts
+++ src/number-to-ptbr.regex.ts
@@ -4,35 +4,49 @@ import { dictionary } from "./dictionary";
  * Converts a number into text representation.
  *
  * @param {number} digit - The number to be converted.
- * @param {Dict} dict - The dictionary containing numeric values as text.
+ * @param {typeof dictionary} dict - The dictionary containing numeric values as text.
  * @returns {string} The text representing the number.
  * @throws {Error} Throws an error if the number is out of the valid range (0-999).
  */
-
 export const convertToText = (digit: number, dict: typeof dictionary): string => {
+  // Check if the input digit is within the valid range (0-999)
   if (digit < 0 || digit > 999) {
     throw new Error("Invalid input");
   }
 
-  if (digit < 20) return dict[digit]; // Return directly for numbers less than 20
+  // Handle numbers less than 20 directly from the dictionary
+  if (digit < 20) {
+    return dict[digit];
+  }
 
-  const numberStr = digit.toString().padStart(3, '0'); // Always consider it a 3-digit number
-
+  // Ensure the number is considered as a 3-digit number
+  const numberStr = digit.toString().padStart(3, '0');
   const hundreds = parseInt(numberStr[0], 10);
   const tens = parseInt(numberStr[1], 10);
   const ones = parseInt(numberStr[2], 10);
 
-  const hundredsText = hundreds > 0 ? dict[hundreds * 100] : "";
-  const tensText = tens > 1 ? dict[tens * 10] : "";
-  const onesText = ones > 0 ? dict[ones] : "";
+  // Initialize variables for textual representation of hundreds, tens, and ones
+  let hundredsText = "";
+  let tensText = "";
+  let onesText = "";
 
-  // If tens is 1, consider it a special case (10 to 19)
+  // Generate textual representation for hundreds, tens, and ones, if applicable
+  if (hundreds > 0) {
+    hundredsText = dict[hundreds * 100];
+  }
+  if (tens > 1) {
+    tensText = dict[tens * 10];
+  }
+  if (ones > 0) {
+    onesText = dict[ones];
+  }
+
+  // Handle special case for tens between 10 and 19
   if (tens === 1) {
     return hundredsText + (hundredsText ? " e " : "") + dict[tens * 10 + ones];
   }
 
+  // Create an array of textual representations and join them with "e"
   const textArray = [hundredsText, tensText, onesText].filter(Boolean);
-
   return textArray.join(" e ");
 };
-
