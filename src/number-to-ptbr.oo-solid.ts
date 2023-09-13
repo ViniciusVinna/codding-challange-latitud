5ea9a547d6a7da2bac65c2dcfa89c39ffbb08215
Joel Garcia Jr <joel.garciajr84@gmail.com>
2023-09-13 18:15:06 -0300

    Replace Magic Numbers with Named Constants in NumberToTextConverter

--- src/number-to-ptbr.oo-solid.ts
+++ src/number-to-ptbr.oo-solid.ts
@@ -2,37 +2,50 @@ import { dictionary } from "./dictionary";
 
 export class NumberToTextConverter {
   private readonly dictionary: Record<number, string> = dictionary;
+  private readonly MIN_RANGE: number = 0;
+  private readonly MAX_RANGE: number = 999;
+  private readonly SINGLE_DIGIT_UPPER_BOUND: number = 20;
+  private readonly TEN: number = 10;
+  private readonly HUNDRED: number = 100;
 
-  convertToText(digit: number): string {
-    if (digit < 0 || digit > 999) {
+  private isInRange(digit: number): boolean {
+    return digit >= this.MIN_RANGE && digit <= this.MAX_RANGE;
+  }
+
+  private isSingleDigit(digit: number): boolean {
+    return digit <= this.SINGLE_DIGIT_UPPER_BOUND || (digit <= this.MAX_RANGE - this.TEN && digit % this.TEN === 0);
+  }
+
+  private convertDoubleDigit(digit: number): string {
+    const tensDigit = Math.floor(digit / this.TEN) * this.TEN;
+    const onesDigit = digit % this.TEN;
+    return this.dictionary[tensDigit] + (onesDigit > this.MIN_RANGE ? ` e ${this.dictionary[onesDigit]}` : "");
+  }
+
+  private convertTripleDigit(digit: number): string {
+    const hundredsDigit = Math.floor(digit / this.HUNDRED) * this.HUNDRED;
+    const remainingDigits = digit % this.HUNDRED;
+
+    if (remainingDigits === this.MIN_RANGE) {
+      return this.dictionary[hundredsDigit];
+    }
+
+    return this.dictionary[hundredsDigit] + " e " + this.convertDoubleDigit(remainingDigits);
+  }
+
+  public convertToText(digit: number): string {
+    if (!this.isInRange(digit)) {
       throw new Error("Invalid input");
     }
 
-    if (digit <= 20 || (digit <= 99 && digit % 10 === 0)) {
+    if (this.isSingleDigit(digit)) {
       return this.dictionary[digit];
     }
 
-    if (digit <= 99) {
-      const tensDigit = Math.floor(digit / 10) * 10;
-      const onesDigit = digit % 10;
-
-      return this.dictionary[tensDigit] + " e " + this.dictionary[onesDigit];
+    if (digit <= this.MAX_RANGE - this.TEN) {
+      return this.convertDoubleDigit(digit);
     }
 
-    if (digit % 100 === 0) {
-      return this.dictionary[digit];
-    }
-
-    const hundredsDigit = Math.floor(digit / 100) * 100;
-    const remainingDigits = digit % 100;
-
-    if (remainingDigits <= 20 || (remainingDigits <= 99 && remainingDigits % 10 === 0)) {
-      return this.dictionary[hundredsDigit] + " e " + this.dictionary[remainingDigits];
-    } else {
-      const tensDigit = Math.floor(remainingDigits / 10) * 10;
-      const onesDigit = remainingDigits % 10;
-
-      return this.dictionary[hundredsDigit] + " e " + this.dictionary[tensDigit] + " e " + this.dictionary[onesDigit];
-    }
+    return this.convertTripleDigit(digit);
   }
 }
